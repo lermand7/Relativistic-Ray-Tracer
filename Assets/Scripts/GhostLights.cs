ded6eed77ddcd911614b124578aac7ceaf46d825
lermand7 <xarma007@gmail.com>
2023-11-01 18:19:32 +0100

    Initial commit

---
+++ Assets/Scripts/GhostLights.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostLights : MonoBehaviour
+{
+    public GameObject BaseLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Clone", 0.1f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Clone()
+    {
+        GameObject clone = Object.Instantiate(BaseLight);
+        clone.transform.position = transform.position;
+        //clone.GetComponent<RayTracedMesh>().
+        clone.SetActive(true);
+    }
+}
